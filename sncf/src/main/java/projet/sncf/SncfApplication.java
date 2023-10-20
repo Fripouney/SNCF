4568ff271e2f2c9dcf98d5c53ed7ccd26cfaaeb3
Fripouney <133747510+Fripouney@users.noreply.github.com>
2023-10-20 08:23:58 +0200

    Création du projet
    Premier commit : initialisation avec Spring initializr

---
+++ sncf/src/main/java/projet/sncf/SncfApplication.java
@@ -0,0 +1,13 @@
+package projet.sncf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SncfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SncfApplication.class, args);
+	}
+
+}
