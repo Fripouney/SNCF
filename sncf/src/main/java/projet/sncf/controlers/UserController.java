3d9c72a5f8255f8b78c94a78ba56b7f975cfda8c
Hugo7764 <133748690+Hugo7764@users.noreply.github.com>
2023-11-08 00:06:13 +0100

    Add files via upload

---
+++ sncf/src/main/java/projet/sncf/controlers/UserController.java
@@ -0,0 +1,53 @@
+package projet.sncf.controlers;
+
+import projet.sncf.repositories.*;
+import projet.sncf.tables.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Get all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Get a single user
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id " + id));
+    }
+    
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id " + id));
+
+        user.setUsername(userDetails.getUsername());
+        user.setPassword(userDetails.getPassword());
+        user.setIsAdmin(userDetails.getIsAdmin());
+
+        return userRepository.save(user);
+    }
+
+    // Delete a user
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
---
+++ sncf/src/main/java/projet/sncf/tables/Train.java
@@ -0,0 +1,34 @@
+package projet.sncf.tables;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Train {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer delay;
+
+    @Column(nullable = false)
+    private Boolean isCanceled;
+
+    @ManyToMany(mappedBy = "trains")
+    private List<Trajet> trajets;
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Integer getDelay() { return delay; }
+    public void setDelay(Integer delay) { this.delay = delay; }
+
+    public Boolean getIsCanceled() { return isCanceled; }
+    public void setIsCanceled(Boolean canceled) { isCanceled = canceled; }
+
+    public List<Trajet> getTrajets() { return trajets; }
+    public void setTrajets(List<Trajet> trajets) { this.trajets = trajets; }
+}
