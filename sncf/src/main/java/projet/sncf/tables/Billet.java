1ed1f597f3bf3a53cbc7a015210d9005a62d9309
Fripouney <133747510+Fripouney@users.noreply.github.com>
2023-11-08 00:34:09 +0100

    Update Billet.java

--- sncf/src/main/java/projet/sncf/tables/Billet.java
+++ sncf/src/main/java/projet/sncf/tables/Billet.java
@@ -24,7 +24,6 @@ public class Billet {
     @Column(nullable = false)
     private Double price;
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
