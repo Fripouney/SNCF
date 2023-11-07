6e73c0a1ae78bebbdac2c86ef97abd7947dd0f81
Fripouney <133747510+Fripouney@users.noreply.github.com>
2023-11-08 00:33:32 +0100

    Update TrajetRepository.java

--- sncf/src/main/java/projet/sncf/repositories/TrajetRepository.java
+++ sncf/src/main/java/projet/sncf/repositories/TrajetRepository.java
@@ -7,10 +7,8 @@ import java.util.List;
 
 public interface TrajetRepository extends JpaRepository<Trajet, Long> {
     
-    // Find a trajet by its start and finish
     List<Trajet> findByStartAndFinish(String start, String finish);
 
-    // Find all trajets for a specific train
     @Query("SELECT tj FROM Trajet tj JOIN tj.trains tr WHERE tr.id = :trainId")
     List<Trajet> findTrajetsByTrainId(Long trainId);
 }
