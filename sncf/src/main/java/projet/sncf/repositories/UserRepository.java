ffede10055d45711f15e249faad2f9545867dfed
Fripouney <133747510+Fripouney@users.noreply.github.com>
2023-11-08 00:33:48 +0100

    Update UserRepository.java

--- sncf/src/main/java/projet/sncf/repositories/UserRepository.java
+++ sncf/src/main/java/projet/sncf/repositories/UserRepository.java
@@ -8,8 +8,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     
     User findByUsernameAndPassword(String username, String password);
-    
-    // An admin might want to get all users for management purposes
-    // This is already provided by JpaRepository.findAll(), but could be explicitly defined if needed.
 }
  
