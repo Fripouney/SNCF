32a3b4ea6e921a122dc8489752c7cd3a5487a778
Fripouney <133747510+Fripouney@users.noreply.github.com>
2023-11-08 00:28:43 +0100

    Update TrainController.java

--- sncf/src/main/java/projet/sncf/controlers/TrainController.java
+++ sncf/src/main/java/projet/sncf/controlers/TrainController.java
@@ -14,38 +14,33 @@ public class TrainController {
     @Autowired
     private TrainRepository trainRepository;
 
-    // Get all trains
     @GetMapping
     public List<Train> getAllTrains() {
         return trainRepository.findAll();
     }
 
-    // Get a single train
     @GetMapping("/{id}")
     public Train getTrainById(@PathVariable Long id) {
         return trainRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Train not found with id " + id));
     }
 
-    // Create a new train
     @PostMapping
     public Train createTrain(@RequestBody Train train) {
         return trainRepository.save(train);
     }
 
-    // Update a train
     @PutMapping("/{id}")
     public Train updateTrain(@PathVariable Long id, @RequestBody Train trainDetails) {
         Train train = trainRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Train not found with id " + id));
 
         train.setDelay(trainDetails.getDelay());
-        train.setIsCanceled(trainDetails.getIsCanceled()); // This now correctly matches your entity
+        train.setIsCanceled(trainDetails.getIsCanceled());
 
         return trainRepository.save(train);
     }
 
-    // Delete a train
     @DeleteMapping("/{id}")
     public void deleteTrain(@PathVariable Long id) {
         Train existingTrain = trainRepository.findById(id)
