fbd757cd83da822a6d1279748178335d7b01357d
Fripouney <133747510+Fripouney@users.noreply.github.com>
2023-11-08 00:34:46 +0100

    Update User.java

--- sncf/src/main/java/projet/sncf/tables/User.java
+++ sncf/src/main/java/projet/sncf/tables/User.java
@@ -23,7 +23,6 @@ public class User {
     @OneToMany(mappedBy = "user")
     private Set<Billet> billets;
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
