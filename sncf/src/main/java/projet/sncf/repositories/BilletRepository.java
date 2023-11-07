e0777b3b7bf57717cf695f44952e2f30562d8ff2
Fripouney <133747510+Fripouney@users.noreply.github.com>
2023-11-08 00:33:02 +0100

    Update BilletRepository.java

--- sncf/src/main/java/projet/sncf/repositories/BilletRepository.java
+++ sncf/src/main/java/projet/sncf/repositories/BilletRepository.java
@@ -6,11 +6,6 @@ import java.util.List;
 
 public interface BilletRepository extends JpaRepository<Billet, Long> {
     
-    // Find all billets for a specific user
     List<Billet> findByUserId(Long long1);
-
-    // Find a specific billet with full details for a user
-    // This method is not strictly necessary because JpaRepository provides one by default (findById),
-    // but if you need to enforce business rules (like ensuring the user owns the billet), you could implement it.
     Billet findByIdAndUserId(Long billetId, Long userId);
 }
